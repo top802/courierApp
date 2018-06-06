087bca59666cca018dce690fd7b4a20b77e4f15d
Oleksander <tatrun89@gmail.com>
2018-06-06 13:38:54 +0200

    add find location GPS

--- app/src/main/java/wisehands/me/deliverty/ProfileActivity.java
+++ app/src/main/java/wisehands/me/deliverty/ProfileActivity.java
@@ -1,7 +1,13 @@
 package wisehands.me.deliverty;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,19 +30,34 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button locsetting;
+    TextView mygpgstatus, mynetstatus, mylocation;
+    private LocationManager locationManager;
+    StringBuilder sbNet = new StringBuilder();
+    private ProfileActivity context = this;
+
     private TextView txtName, txtEmail;
     private FirebaseAuth mAuth;
 
-    private GoogleApiClient mGoogleApiClient;
+    private final String TOPIC = "JavaSampleApproach";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mylocation = (TextView) findViewById(R.id.mylocation);
+        mynetstatus = (TextView) findViewById(R.id.netstatus);
+        mygpgstatus = (TextView) findViewById(R.id.gpgstatus);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        findViewById(R.id.locSetting).setOnClickListener(this);
+
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC);
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
@@ -51,6 +72,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         findViewById(R.id.bn_cancel).setOnClickListener(this);
 
         FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+
         final ProfileActivity  self = this;
         mUser.getIdToken(true)
                 .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
@@ -62,7 +84,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
                             // Instantiate the RequestQueue.
                             RequestQueue queue = Volley.newRequestQueue(self);
-                            String url ="http://192.168.1.3:8080/authenticate?token=" + idToken;
+                            String url ="http://192.168.0.15:8080/authenticate?token=" + idToken;
 
                             // Request a string response from the provided URL.
                             StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -93,10 +115,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                         }
                     }
                 });
-
-
     }
-
 
     @Override
     public void onClick(View view) {
@@ -107,8 +126,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.bn_cancel:
                 cancelOrder();
             break;
-
-
+            case R.id.locSetting:
+                startActivity(new Intent(
+                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            break;
         }
     }
 
@@ -121,6 +142,103 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         Toast.makeText(ProfileActivity.this, "You are canceled delivery", Toast.LENGTH_SHORT).show();
     }
 
+    // start get location
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000 * 10, 10, locationListener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 1000 * 1, 10,
+                locationListener);
+        checkEnabled();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            checkEnabled();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            checkEnabled();
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            showLocation(locationManager.getLastKnownLocation(provider));
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (provider.equals(LocationManager.GPS_PROVIDER)) {
+                mygpgstatus.setText("GPS status: " + String.valueOf(status));
+            } else
+            if (provider.equals(LocationManager.NETWORK_PROVIDER)) {
+                mynetstatus.setText("NET status: " + String.valueOf(status));
+            }
+        }
+    };
+
+    private void showLocation(Location location) {
+        if (location == null)
+            return;
+        if (location.getProvider().equals(LocationManager.GPS_PROVIDER)) {
+            mylocation.setText(formatLocation(location));
+        } else
+        if (location.getProvider().equals(
+                LocationManager.NETWORK_PROVIDER)) {
+            mylocation.setText(formatLocation(location));
+        }
+    }
+
+    private String formatLocation(Location location) {
+        if (location == null)
+            return "";
+        return String.format(
+                "Coordinates: lat = %1$.4f, lon = %2$.4f",
+                location.getLatitude(), location.getLongitude());
+    }
+
+    private void checkEnabled() {
+        mygpgstatus.setText("GPS enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER));
+        mynetstatus.setText("NET enabled: "
+                + locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER));
+    }
 
 }
 
